f50d6384883ea627c398d24dabb79e7abb1043ed
Szymon Ryczek <szymon1177@gmail.com>
2020-01-05 22:17:47 +0100

    Api Create FromBuilder

--- Verify-Your-Business/MainWindow.xaml.cs
+++ Verify-Your-Business/MainWindow.xaml.cs
@@ -18,11 +18,92 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
+using System.Reflection;
+
+
 namespace Verify_Your_Business
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
+    /// 
+
+    public class TaxPayerFormFields
+    {
+        public string name { get; }
+        public string nip { get; }
+        public string statusVat { get; }
+        public string regon { get; }
+        public string pesel { get; }
+        public string krs { get; }
+        public string residenceAddress { get; }
+        public string workingAddress { get; }
+        public string representatives { get; }
+        public string authorizedClerks { get; }
+        public string partners { get; }
+        public string accountNumbers { get; }
+        public string registrationLegalDate { get; }
+        // public string registrationDenialBasis { get; }
+        public string registrationDenialDate { get; }
+        // public string restorationBasis { get; }
+        public string restorationDate { get; }
+        public string removalBasis { get; }
+        // public string removalDate { get; }
+
+        public TaxPayerFormFields()
+        {
+            name = "Firma (nazwa) lub imię i nazwisko";
+            nip = "Numer, za pomocą którego podmiot został zidentyfikowany na potrzeby podatku, jeżeli taki numer został przyznany";
+            statusVat = "Status podatnika (wg stanu na dzień sprawdzenia)";
+            regon = "Numer identyfikacyjny REGON, o ile został nadany";
+            pesel = "Numer PESEL, o ile podmiot posiada:";
+            krs = "Numer w Krajowym Rejestrze Sądowym, o ile został nadany:";
+            residenceAddress = "Adres siedziby – w przypadku podmiotu niebędącego osobą fizyczną:";
+            workingAddress = "Adres stałego miejsca prowadzenia działalności albo adres miejsca zamieszkania, w przypadku braku adresu stałego miejsca prowadzenia działalności - w odniesieniu do osoby fizycznej:";
+            representatives = "Imiona i nazwiska prokurentów oraz ich numery identyfikacji podatkowej lub numery PESEL:";
+            authorizedClerks = "Imiona i nazwiska osób wchodzących w skład organu uprawnionego do reprezentowania podmiotu oraz ich numery identyfikacji podatkowej lub numery PESEL:";
+            partners = "Imię i nazwisko lub firma (nazwa) wspólnika oraz jego numer identyfikacji podatkowej lub numer PESEL:";
+            accountNumbers = "Numery rachunków rozliczeniowych lub imiennych rachunków w SKOK:";
+            registrationLegalDate = "Data rejestracji jako podatnika VAT:";
+            // registrationDenialBasis = ": " + registrationDenialBasis;
+            registrationDenialDate = "Data odmowy rejestracji jako podatnika VAT Podstawa prawna odmowy rejestracji:";
+            // restorationBasis = ": " + restorationBasis;
+            restorationDate = "Data przywrócenia rejestracji jako podatnika VAT Podstawa prawna przywrócenia:";
+            removalBasis = "Data wykreślenia rejestracji jako podatnika VAT Podstawa prawna wykreślenia:";
+            // removalDate = ": " + removalDate;
+        }
+    }
+
+    public class Helper
+    {
+        public List<KeyValuePair<string, string>> FormFieldsList = new List<KeyValuePair<string, string>>();
+
+        public Helper(JObject apiResponse)
+        {
+            TaxPayerFormFields taxPayerFormFields = new TaxPayerFormFields();
+            PropertyInfo[] properties = taxPayerFormFields.GetType().GetProperties();
+
+            foreach (JProperty element in (JToken)apiResponse["result"]["subject"])
+            {
+                var matches = from val in properties where val.Name == element.Name select val;
+
+                foreach (var match in matches)
+                {
+                    Type elementType = element.Value.GetType();
+                    string label = (string)match.GetValue(taxPayerFormFields, null);
+
+                    if (elementType.Name == "JArray")
+                    {
+                        string el = string.Join(",", element.Value);
+                        FormFieldsList.Add(new KeyValuePair<string, string>(label, el));
+                    } else {
+                        FormFieldsList.Add(new KeyValuePair<string, string>(label, (string)element.Value));
+                    }
+                }
+            }
+        }
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -38,81 +119,30 @@ namespace Verify_Your_Business
             // todo
             string search1 = text_search.Text;
             string search2 = date_search.Text;
-
-
             HttpClient client = new HttpClient();
+
             client.BaseAddress = new Uri(URL);
-
-            // Add an Accept header for JSON format.
-
             client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-
-
-            // List data response.
-            HttpResponseMessage response = client.GetAsync(search1 + "?date=2019-09-02").Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            HttpResponseMessage response = client.GetAsync(search1 + "?date=2019-09-02").Result;
             if (response.IsSuccessStatusCode)
             {
                 string responseString = response.Content.ReadAsStringAsync().Result;
                 JObject jObject = JObject.Parse(responseString);
+                Helper helper = new Helper(jObject);
 
-                string name = (string)jObject["result"]["subject"]["name"];
-                string nip = (string)jObject["result"]["subject"]["nip"];
-                string statusVat = (string)jObject["result"]["subject"]["statusVat"];
-                string regon = (string)jObject["result"]["subject"]["regon"];
-                string pesel = (string)jObject["result"]["subject"]["pesel"];
-                string krs = (string)jObject["result"]["subject"]["krs"];
-                string residenceAddress = (string)jObject["result"]["subject"]["residenceAddress"];
-                string workingAddress = (string)jObject["result"]["subject"]["workingAddress"];
-                string representatives = string.Join("", jObject["result"]["subject"]["representatives"]);
-                string authorizedClerks = string.Join("", jObject["result"]["subject"]["authorizedClerks"]);
-                string partners = string.Join("", jObject["result"]["subject"]["partners"]);
-                string accountNumbers = string.Join("", jObject["result"]["subject"]["accountNumbers"]);
-                string registrationLegalDate = (string)jObject["result"]["subject"]["registrationLegalDate"];
-                string registrationDenialBasis = (string)jObject["result"]["subject"]["registrationDenialBasis"];
-                string registrationDenialDate = (string)jObject["result"]["subject"]["registrationDenialDate"];
-                string restorationBasis = (string)jObject["result"]["subject"]["restorationBasis"];
-                string restorationDate = (string)jObject["result"]["subject"]["restorationDate"];
-                string removalBasis = (string)jObject["result"]["subject"]["removalBasis"];
-                string removalDate = (string)jObject["result"]["subject"]["removalDate"];
-                
-                name = "Firma (nazwa) lub imię i nazwisko: " + name;
-                nip = "Numer, za pomocą którego podmiot został zidentyfikowany na potrzeby podatku, jeżeli taki numer został przyznany: " + nip;
-                statusVat = "Status podatnika (wg stanu na dzień sprawdzenia): " + statusVat;
-                regon = "Numer identyfikacyjny REGON, o ile został nadany: " + regon;
-                pesel = "Numer PESEL, o ile podmiot posiada:" + pesel;
-                krs = "Numer w Krajowym Rejestrze Sądowym, o ile został nadany: " + krs;
-                residenceAddress = "Adres siedziby – w przypadku podmiotu niebędącego osobą fizyczną: " + residenceAddress;
-                workingAddress = "Adres stałego miejsca prowadzenia działalności albo adres miejsca zamieszkania, w przypadku braku adresu stałego miejsca prowadzenia działalności - w odniesieniu do osoby fizycznej: " + workingAddress;
-                representatives = "Imiona i nazwiska prokurentów oraz ich numery identyfikacji podatkowej lub numery PESEL: " + representatives;
-                authorizedClerks = "Imiona i nazwiska osób wchodzących w skład organu uprawnionego do reprezentowania podmiotu oraz ich numery identyfikacji podatkowej lub numery PESEL: " + authorizedClerks;
-                partners = "Imię i nazwisko lub firma (nazwa) wspólnika oraz jego numer identyfikacji podatkowej lub numer PESEL: " + partners;
-                accountNumbers = "Numery rachunków rozliczeniowych lub imiennych rachunków w SKOK: " + accountNumbers;
-                registrationLegalDate = "Data rejestracji jako podatnika VAT: " + registrationLegalDate;
-                //  registrationDenialBasis = ": " + registrationDenialBasis;
-                registrationDenialDate = "Data odmowy rejestracji jako podatnika VAT Podstawa prawna odmowy rejestracji: " + registrationDenialDate;
-                // restorationBasis = ": " + restorationBasis;
-                restorationDate = "Data przywrócenia rejestracji jako podatnika VAT Podstawa prawna przywrócenia " + restorationDate;
-                removalBasis = "Data wykreślenia rejestracji jako podatnika VAT Podstawa prawna wykreślenia: " + removalBasis;
-                // removalDate = ": " + removalDate;
-
-                tbSettingText.Content = name + "\n" + nip + "\n" + statusVat + "\n" + regon + "\n" + pesel + "\n" + krs + "\n" + residenceAddress + "\n" 
-                    + workingAddress + "\n" + representatives + "\n" + authorizedClerks + "\n" + partners + "\n" + accountNumbers + "\n" + registrationLegalDate + "\n"
-                    + registrationDenialDate + "\n" + restorationDate + "\n" + removalBasis;
+                foreach (KeyValuePair<string, string> field in helper.FormFieldsList)
+                {
+                     tbSettingText.Content += field.Key + ": " + field.Value + "\n";
+                }
             }
             else
             {
                 Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
             }
 
-            //Make any other calls using HttpClient here.
-
-            //Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
             client.Dispose();
-
-           // tbSettingText.Content = "TODO";
         }
     }
 }
