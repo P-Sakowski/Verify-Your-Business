bea78099e276dc75934a48c697e64f98ff1cb937
unknown <maks.stepak1@gmail.com>
2020-01-11 22:28:54 +0100

    Add Unit Tests Project

---
+++ Verify-Your-Business-UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Verify_Your_Business_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
